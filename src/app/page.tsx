c2ded223da62d683a7849778bbd015e35ec014f4
aayush-wiz <aayush286g@gmail.com>
2025-10-25 11:01:45 +0530

    database configured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
-const Page = () => {
+import prisma from "@/lib/db";
+
+const Page = async () => {
+  const users = await prisma.user.findMany();
   return (
-    <div className="text-3xl font-bold underline bg-amber-500">Hello World</div>
+    <div className="text-3xl flex flex-col items-center justify-center h-screen bg-amber-500">
+      {JSON.stringify(users[0].id)}
+    </div>
   );
 };
 
