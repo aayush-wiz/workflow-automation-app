c9b231738ccabd4515046ea2b9ffd08800bb387f
aayush-wiz <aayush286g@gmail.com>
2025-10-28 18:46:55 +0530

    Inngest for background jobs and mprocs for running multiple processes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
-import LogoutButton from "./logout";
+"use client";
 
-const Page = async () => {
-  await requireAuth();
-  const data = await caller.getUsers();
+import LogoutButton from "./logout";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
   return (
-    <div>
+    <div className="flex flex-col gap-4 justify-center items-center h-screen">
       <h1>Protected Page</h1>
 
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-
+      <pre className="text-md text-gray-500">
+        {JSON.stringify(data, null, 2)}
+      </pre>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
       <LogoutButton />
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,13 +1,19 @@
 import prisma from "@/lib/db";
 import { createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inggest/client";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany({
-      where: {
-        id: ctx.session.user.id,
+  getWorkflows: protectedProcedure.query(() => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "aayush@gmail.com",
       },
     });
+    return { success: true, message: "Job Queued" };
   }),
 });
 // export type definition of API
